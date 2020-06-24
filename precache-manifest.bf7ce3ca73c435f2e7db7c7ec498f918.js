184150109394567b2c3d24ce707b10d7a88fd344
dkershner6 <dkershner@cbtnuggets.com>
2020-06-24 20:09:07 +0000

    Deploying to gh-pages from  @ 43e6c0091a317c44a7ca5150fa9b628bd103fc34 🚀

---
+++ precache-manifest.bf7ce3ca73c435f2e7db7c7ec498f918.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "202534b1a4543a0ea24bbcdc598737f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "533d851c6483223ae2f5",
+    "url": "/static/css/main.6a833c0f.chunk.css"
+  },
+  {
+    "revision": "e4dfd4de74a336b183bd",
+    "url": "/static/js/2.76dd4d91.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.76dd4d91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "533d851c6483223ae2f5",
+    "url": "/static/js/main.1775187a.chunk.js"
+  },
+  {
+    "revision": "35fc9237a649ee51992c",
+    "url": "/static/js/runtime-main.bf6f56a0.js"
+  }
+]);
